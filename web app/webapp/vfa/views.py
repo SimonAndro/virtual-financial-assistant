b16fccc947bbabd609639f02981d211b1837aa1c
Turi <40395348+turinaf@users.noreply.github.com>
2024-01-22 16:39:03 +0800

    ignore cache

--- web app/webapp/vfa/views.py
+++ web app/webapp/vfa/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from . import crypto
+# from . import crypto
 
 from vfa.models import Account
 
